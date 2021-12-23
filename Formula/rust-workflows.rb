46cce32187c082bc31e5866d4847831c050f4d2e
Hendrik Maus <188284+hendrikmaus@users.noreply.github.com>
2021-12-23 13:20:48 +0000

    rust-workflows: add 0.3.1_1 bottle.

--- Formula/rust-workflows.rb
+++ Formula/rust-workflows.rb
@@ -11,6 +11,12 @@ class RustWorkflows < Formula
     strategy :github_latest
   end
 
+  bottle do
+    root_url "https://github.com/hendrikmaus/homebrew-tap/releases/download/rust-workflows-0.3.1_1"
+    sha256 cellar: :any_skip_relocation, big_sur:      "050151fb4d61a0526cefb0e908c147126134562d487f8714c2fddb93c02e7a52"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d6629df495ea8fc792f55e7e0915ff297b21f717f058c8ff49f4cd89fac33294"
+  end
+
   head do
     url "https://github.com/hendrikmaus/rust-workflows.git", branch: "master"
   end
