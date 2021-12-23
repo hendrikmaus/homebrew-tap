a178928f7f2dfe70cbeac12bb5475e5e77d4beab
Hendrik Maus <hendrikmaus@users.noreply.github.com>
2021-12-23 14:18:21 +0100

    Remove bottles from the helm-templexer formula

--- Formula/helm-templexer.rb
+++ Formula/helm-templexer.rb
@@ -11,12 +11,6 @@ class HelmTemplexer < Formula
     strategy :github_latest
   end
 
-  bottle do
-    root_url "https://github.com/hendrikmaus/homebrew-tap/releases/download/helm-templexer-2.0.0_1"
-    sha256 cellar: :any_skip_relocation, catalina:     "bc70c85aba76810d25ca17847b4fd1093c0d20bcfa258680c0e4b8ba43306eea"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "98de6da2608a9f93047db02a366239f010b54642f58fe1c0629b1a98dddf5060"
-  end
-
   head do
     url "https://github.com/hendrikmaus/helm-templexer.git"
   end
