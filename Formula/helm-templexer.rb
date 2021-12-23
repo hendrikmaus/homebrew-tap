9fb3d7bbafaac92b24e680176124a8167950f7bc
Hendrik Maus <188284+hendrikmaus@users.noreply.github.com>
2021-12-23 13:33:35 +0000

    helm-templexer: add 2.0.0_1 bottle.

--- Formula/helm-templexer.rb
+++ Formula/helm-templexer.rb
@@ -11,6 +11,13 @@ class HelmTemplexer < Formula
     strategy :github_latest
   end
 
+  bottle do
+    root_url "https://github.com/hendrikmaus/homebrew-tap/releases/download/helm-templexer-2.0.0_1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, big_sur:      "f1268439a67c0b41f0c71841f7d03ad7e8634acb6c29e7ee4e217305b2e20870"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "f97f359f398438cb8fda482be816fbb03002600531565a8e187ca1cf9011f70c"
+  end
+
   head do
     url "https://github.com/hendrikmaus/helm-templexer.git"
   end
